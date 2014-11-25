b9622adebbd53f62b922770c5a15bf70779523ff
zonage <zhirui10@hotmail.com>
2014-11-25 01:49:27 -0500

    Added files

---
+++ coop.h
@@ -0,0 +1,16 @@
+#ifndef __COOPFEE_H__
+#define __COOPFEE_H__
+#include <iostream>
+#include "nonproperty.h"
+#include "player.h"
+
+
+class CoopFee: public non-property
+{
+public:
+	player * occupying;
+	virtual void action();
+	~CoopFee();
+};
+
+#endif
---
+++ slc.cc
@@ -0,0 +1,11 @@
+#include "slc.h"
+
+SLC::action() 
+{
+	//player is randomly moved on the board or gets a rim card
+}
+
+SLC::~SLC()
+{
+	delete occupying;
+}
