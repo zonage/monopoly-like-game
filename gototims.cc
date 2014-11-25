83db4888f8b6a5b904463dbd9ec92a88be0b33d5
zonage <zhirui10@hotmail.com>
2014-11-25 02:19:59 -0500

    Added comments to gototims.h

--- gototims.cc
+++ gototims.cc
@@ -1,10 +1,12 @@
 #include "gototims.h"
 
+// changes the player's position to the Tim's Line
 GoToTims::action()
 {
 	occupying->position = theBoard.squareArr[10];
 }
 
+// destructor
 GoToTims::~GoToTims()
 {
 	delete occupying;
